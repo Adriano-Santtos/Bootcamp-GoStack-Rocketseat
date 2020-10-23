71b15983c5070978971d91599756a38524809a9b
Adriano Santtos <adrianosantosx2@hotmail.com>
2020-10-23 05:07:47 -0300

    update

--- nivel-1/backend-com-node/src/index.js
+++ nivel-1/backend-com-node/src/index.js
@@ -1,1 +1,12 @@
-console.log('hello')
+const express = require ('express');
+
+const app = express ();
+
+
+app.get('/', (request, response) => {
+    return response.json({
+        message: 'Hello man'
+    });
+});
+
+app.listen( 3333);
