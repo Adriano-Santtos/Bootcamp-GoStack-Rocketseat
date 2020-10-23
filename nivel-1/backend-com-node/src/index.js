219aaa4c9545b92818075df063e0ee362d6c9b18
Adriano Santtos <adrianosantosx2@hotmail.com>
2020-10-23 05:18:02 -0300

    update

--- nivel-1/backend-com-node/src/index.js
+++ nivel-1/backend-com-node/src/index.js
@@ -3,10 +3,42 @@ const express = require ('express');
 const app = express ();
 
 
-app.get('/', (request, response) => {
-    return response.json({
-        message: 'Hello man'
-    });
+
+app.get('/projects', (request, response) => {
+    return response.json ([
+        'projeto 1',
+        'projeto 2'
+    ]);
 });
 
+app.post('/projects', (request, response) => {
+    return response.json([
+        'Projeto 1',
+        'Projeto 2',
+        'Projeto 3'
+    ]);
+});
+
+app.put('/projects/:id', (request, response) => {
+    return response.json([
+        'Projeto 4',
+        'Projeto 2',
+        'Projeto 3'
+    ]);
+});
+
+
+app.delete('/projects/:id', (request, response) => {
+    return response.json([
+      
+        'Projeto 2',
+        'Projeto 3'
+    ]);
+});
+
+
+
+
+
 app.listen( 3333);
+
