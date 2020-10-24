ef387125152a27428f1c02976d4c6f5b7e8bacef
Adriano Santtos <adrianosantosx2@hotmail.com>
2020-10-24 17:06:28 -0300

    update

--- nivel-1/backend-com-node/src/index.js
+++ nivel-1/backend-com-node/src/index.js
@@ -1,19 +1,48 @@
 const express = require ('express');
 
-const  { uuid } = require ('uuidv4');  // cria um id únido para cada projeto
+const  { uuid, isUuid } = require ('uuidv4');  // cria um id únido para cada projeto
 
 const app = express ();
 
 app.use(express.json());
-
+app.use('/projects/:id ', validadeProjectId);
 
 const projects = []; //usada par armazenar os dados da apliccação, jamais utilizar em produção
 
+function logRequests(request, response, next) {
+    const { method, url } = request;
+
+    const logLabel = `[${method.toUpperCase()}] ${url}`;
+
+    
+    console.log('1');
+    console.time(logLabel);
+    
+    next(); // Próximo middleware
+    
+    console.log('2');
+    console.timeEnd(logLabel);
+    // aqui (passo 3)
+}
+
+function validadeProjectId(request, response, next){
+    const {id} = request.params;
+
+    if (!isUuid (id)){
+        return response.status(400).json({ error: 'Invalid project ID. '});
+
+     }
+     return next();
+
+}
 
 
+app.use(logRequests);
+app.use('/projects/:id', validadeProjectId);
 
-app.get('/projects', (request, response) => {
 
+app.get('/projects',  (request, response) => {
+    console.log('3');
   const {title} = request.query; //filtro criado para title
 
   const results = title 
@@ -23,10 +52,8 @@ app.get('/projects', (request, response) => {
 });
 
 
-
-
-
 app.post('/projects', (request, response) => {
+
     const {title, owner} = request.body;
 
     const project = { id: uuid(), title, owner }; // criando um único projeto / te liga que ali foi usado só o id
@@ -36,9 +63,6 @@ app.post('/projects', (request, response) => {
     return response.json(project); // aqui eu exibo o projeto recem criado, e não a lista toda;
 
 });
-
-
-
 
 
 app.put('/projects/:id', (request, response) => {
@@ -63,9 +87,6 @@ app.put('/projects/:id', (request, response) => {
 });
 
 
-
-
-
 app.delete('/projects/:id', (request, response) => {
     const {id} = request.params;
     const projectIndex = projects.findIndex(project => project.id === id);
@@ -80,9 +101,8 @@ app.delete('/projects/:id', (request, response) => {
 });
 
 
+console.log('Backend started 🚓')
 
-console.log('Backend started 🏍')
 
-
-app.listen( 3333);
+app.listen(3333);
 
