73de165b1c77dd5e9917ef4b326c01cd1733ed29
Adriano Santtos <adrianosantosx2@hotmail.com>
2020-10-24 10:17:58 -0300

    update

--- nivel-1/backend-com-node/src/index.js
+++ nivel-1/backend-com-node/src/index.js
@@ -1,55 +1,88 @@
 const express = require ('express');
 
+const  { uuid } = require ('uuidv4');  // cria um id únido para cada projeto
+
 const app = express ();
 
 app.use(express.json());
 
 
+const projects = []; //usada par armazenar os dados da apliccação, jamais utilizar em produção
+
+
+
+
+
 app.get('/projects', (request, response) => {
 
-    const query = request.query;
-    console.log(query);
+  const {title} = request.query; //filtro criado para title
 
-    return response.json ([
-        'projeto 1',
-        'projeto 2'
-    ]);
+  const results = title 
+    ? projects.filter(project => project.title.includes(title))
+    : projects; //se não tem o que foi filtrado ele retorna todos os projetoc
+    return response.json (results);
 });
+
+
+
+
 
 app.post('/projects', (request, response) => {
-    const body = request.body
+    const {title, owner} = request.body;
 
-    console.log(body);
+    const project = { id: uuid(), title, owner }; // criando um único projeto / te liga que ali foi usado só o id
 
-    return response.json([
-        'Projeto 1',
-        'Projeto 2',
-        'Projeto 3'
-    ]);
+    projects.push(project); //.push ta jogando informação para dentro do array
+
+    return response.json(project); // aqui eu exibo o projeto recem criado, e não a lista toda;
+
 });
+
+
+
+
 
 app.put('/projects/:id', (request, response) => {
-    const params = request.params;
-    console.log(params);
+    const {id} = request.params;
+    const {title, owner} = request.body;
     
-    return response.json([
-        'Projeto 4',
-        'Projeto 2',
-        'Projeto 3'
-    ]);
+    const projectIndex  = projects.findIndex(project => project.id === id ); //compara para ver se o projeto é o id 
+
+    if (projectIndex <0 ) { //se ele não encontrou o indice < 0
+        return response.status(400).json({ error: 'project not found'}); 
+    }
+
+    const project = {
+        id,
+        title,
+        owner,
+    };
+
+    projects[projectIndex] = project;
+
+    return response.json(project);
 });
+
+
+
 
 
 app.delete('/projects/:id', (request, response) => {
-    return response.json([
-      
-        'Projeto 2',
-        'Projeto 3'
-    ]);
+    const {id} = request.params;
+    const projectIndex = projects.findIndex(project => project.id === id);
+
+    if (projectIndex < 0) {
+        return response.status(400).json({ error: 'Project not found.'}
+        )
+    }
+    projects.splice(projectIndex, 1);
+
+    return response.status(204).send(); 
 });
 
 
 
+console.log('Backend started 🏍')
 
 
 app.listen( 3333);
