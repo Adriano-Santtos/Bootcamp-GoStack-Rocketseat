ce1c21ce9d09f656186ffd92b5a5910fcd181e30
Adriano Santtos <adrianosantosx2@hotmail.com>
2020-11-05 20:16:11 -0300

    update

--- nivel-1/frontend-com-react/src/App.js
+++ nivel-1/frontend-com-react/src/App.js
@@ -1,31 +1,36 @@
-import React, { useState} from 'react';
+import React, { useState, useEffect} from 'react';
 
-import backgroundImage from './assets/background.jpg'
 import './App.css';
+
+import api from './services/api';
 
 import Header from './components/Header';
 
 function App () {
-    const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end Web']);
+    const [projects, setProjects] = useState([]);
+ 
+    useEffect(() => {
+      api.get('projects').then(response => {//api.get para buscar o get no backend
+          setProjects(response.data);
+      });
+    }, []);//array de dependências
 
-    function handleAddProject () { 
-        setProjects([...projects, `Novo Projeto ${Date.now()}`]),
-
-        console.log(projects);
+    function handleAddProject() {
+        setProjects([...projects, `Novo Projeto ${Date.now()}`]);
     }
+
 
     return (
         <>
         <Header title = "Projects"/>
-        <img width={500} src = {backgroundImage} />
-
+        
         <ul>
-            {projects.map(project => <li key = { project}>{project}</li>)}
+            {projects.map(project => <li key={project.id}>{project.title}</li>)}
         </ul>
 
         <button type="button" onClick={handleAddProject}>Adicionar Projeto</button>
         </>
     );
-}
+};
 
 export default App;
