79dfd9da0684a21918002cbf6f0861733c4844d4
Adriano Santtos <adrianosantosx2@hotmail.com>
2020-12-05 10:41:02 -0300

    configuring typescript

--- Module-03.1-TypeScript/src/index.ts
+++ Module-03.1-TypeScript/src/index.ts
@@ -1,10 +1,10 @@
-import express from 'express';
+import express, { response } from 'express';
 
 const app = express();
 
 app.get('/', (request, response) => {
-    return response.json({message:'Hello World'});
-})
+    return response.
+});
 
 app.listen(3333);
 
