08428de94385fa5fb95a298a66adc462c1e8469b
Adriano Santtos <adrianosantosx2@hotmail.com>
2020-12-13 16:26:10 -0300

    register

--- Module-04-Firs-Project-with-Node.JS/src/routes/index.ts
+++ Module-04-Firs-Project-with-Node.JS/src/routes/index.ts
@@ -1,17 +1,9 @@
 import { Router } from 'express';
+import appointmentsRouter from './appointments.routes'
 
 const routes = Router();
 
-routes.post('/users', (request, response) => {
+routes.use('/appointments', appointmentsRouter); // o .use funciona para qualquer tipo de rota. Aqui ta jogando as coisas para appointme
 
-    const{ name, email} = request.body;
-
-    const user = {
-        name, 
-        email,
-    };
-
-    return response.json(user);
-});
 
 export default routes;
