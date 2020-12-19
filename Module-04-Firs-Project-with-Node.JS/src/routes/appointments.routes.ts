ae66d76197983eafd8fee41df18347bbc9e216a1
Adriano Santtos <adrianosantosx2@hotmail.com>
2020-12-19 16:21:12 -0300

    validating the date

--- Module-04-Firs-Project-with-Node.JS/src/routes/appointments.routes.ts
+++ Module-04-Firs-Project-with-Node.JS/src/routes/appointments.routes.ts
@@ -1,9 +1,40 @@
 import { Router } from "express";
+import {uuid} from 'uuidv4';
+import { startOfHour, parseISO, isEqual} from 'date-fns';
 
 const appointmentsRouter = Router();
 
+interface appointment{
+    id: string,
+    provider: string,
+    date: Date, 
+}
+
+const appointments: appointment[] = [];
+
 appointmentsRouter.post('/', (request, response)=>{
+    const { provider, date } = request.body;
+
+    const parsedDate = startOfHour(parseISO(date));
+
+    const findAppointmentInSameDate = appointments.find(appointment => 
+        isEqual(parsedDate, appointment.date));
+
+        if(findAppointmentInSameDate){
+            return response.status(400).json({message: "This appointment is already booked"});
+        }
+       
     return response.json({ message: 'hello world'});
-});
+
+    const appointment = {
+        id: uuid(),
+        provider,
+        date: parsedDate,
+    };
+
+    appointments.push(appointment);
+
+    return response.json(appointment);
+}); 
 
 export default appointmentsRouter;
