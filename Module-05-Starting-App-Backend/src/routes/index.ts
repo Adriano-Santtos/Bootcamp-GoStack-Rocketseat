18440054a3b6a25b1c675ea152513e4aa628715a
Adriano Santtos <adrianosantosx2@hotmail.com>
2021-02-03 21:29:24 -0300

    user routes added

--- Module-05-Starting-App-Backend/src/routes/index.ts
+++ Module-05-Starting-App-Backend/src/routes/index.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import appointmentsRouter from './appointments.routes'
+import usersRouter from './users.routes'
 
 const routes = Router();
 
 routes.use('/appointments', appointmentsRouter); // o .use funciona para qualquer tipo de rota. Aqui ta jogando as coisas para appointme
-
+routes.use('/users', usersRouter); // o .use funciona para
 
 export default routes;
 
