07e70494b0f1f65073983af2e4ae51cc197eb4c7
Adriano Santtos <adrianosantosx2@hotmail.com>
2021-02-11 21:25:19 -0300

    Generating JWT token

--- Module-05-Starting-App-Backend/src/services/AuthenticateUserService.ts
+++ Module-05-Starting-App-Backend/src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import {getRepository} from 'typeorm';
 import {compare} from 'bcryptjs';
+import {sign} from 'jsonwebtoken';
 // import { hash } from 'bcryptjs';
 import User from '../models/User';
 
@@ -8,13 +9,18 @@ interface Request {
     password: string;
 }
 
+interface Response{
+    user: User;
+    token: string;
+
+}
+
 class AuthenticateUserService {
-    public async execute({email, password}: Request): Promise<user>{
+    public async execute({email, password}: Request): Promise<Response>{
 
         const usersRepository = getRepository(User);
 
         const user = await usersRepository.findOne({where:{email}});
-
         if(!user) {
             throw new Error ('incorret email/password combination');
         }
@@ -25,9 +31,15 @@ class AuthenticateUserService {
             throw new Error('incorret email/password combination');
         }
 
+        const token = sign({},'7ffc30ea5de5606bd799f31ad3f94761', {
+            subject:user.id, // sempre será o id do usuário que gerou o tokien
+            expiresIn:'1d', //quanto tempo que o tokien vai durar
+        });
+
      
         return {
             user,
+            token,
         };
     }
 }
