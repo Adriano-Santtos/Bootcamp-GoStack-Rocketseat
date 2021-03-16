bb0b40bf17bcbb4236b17095efdb1bc4a4dc0dba
Adriano Santtos <adrianosantosx2@hotmail.com>
2021-03-16 17:24:29 -0300

    saving to storage

--- Module-06-First-Project-React/src/pages/Dashboard/index.tsx
+++ Module-06-First-Project-React/src/pages/Dashboard/index.tsx
@@ -2,7 +2,7 @@ import logoImg from '../../assets/logo.svg';
 import {FiChevronRight} from 'react-icons/fi'
 import api from '../../services/api';
 
-import React,{useState, FormEvent} from 'react';
+import React,{useState, FormEvent, useEffect} from 'react';
 
 import { Title, Form, Error, Repositories } from './styles';
 
@@ -21,7 +21,25 @@ const Dashboard: React.FC = () => {
 
   const [newRepo, setNewRepo] = useState('');
   const [inputError, setInputError] = useState('');
-  const [repositories, setRepositories] = useState<Repository[]>([]);
+  const [repositories, setRepositories] = useState<Repository[]>(() => {
+    const storageRepositories = localStorage.getItem(
+      '@GithubExplorer:repositories',
+    );
+
+    if (storageRepositories) {
+      return JSON.parse(storageRepositories);
+    }
+
+    return [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem(
+      '@GithubExplorer:repositories',
+      JSON.stringify(repositories),
+    );
+  }, [repositories]);
+
 
   async function handleAddRepository(
     e: FormEvent<HTMLFormElement>,
