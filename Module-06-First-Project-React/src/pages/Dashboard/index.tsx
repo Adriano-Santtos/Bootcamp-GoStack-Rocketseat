676b9046354616e1b39e23d3cce1c6c411b64b43
Adriano Santtos <adrianosantosx2@hotmail.com>
2021-03-16 17:53:43 -0300

    navigating between routes

--- Module-06-First-Project-React/src/pages/Dashboard/index.tsx
+++ Module-06-First-Project-React/src/pages/Dashboard/index.tsx
@@ -1,5 +1,6 @@
 import logoImg from '../../assets/logo.svg';
 import {FiChevronRight} from 'react-icons/fi'
+import {Link} from 'react-router-dom';
 import api from '../../services/api';
 
 import React,{useState, FormEvent, useEffect} from 'react';
@@ -89,7 +90,10 @@ const Dashboard: React.FC = () => {
       <Repositories>
 
       {repositories.map(repository => (
-          <a key={repository.full_name} href="teste">
+          <Link
+          key={repository.full_name}
+          to={`/repositories/${repository.full_name}`}
+        >
             <img
               src={repository.owner.avatar_url}
               alt={repository.owner.login}
@@ -100,7 +104,9 @@ const Dashboard: React.FC = () => {
             </div>
 
             <FiChevronRight size={20} />
-          </a>
+
+          </Link>
+          
         ))}
         
 
